dfb1468645c4d9e500c0cef991c955a7e9717bfe
Pro <pranavvyas4399@gmail.com>
2018-08-08 13:49:54 +0530

    Completed Project Espresso Done

---
+++ app/src/androidTest/java/com/vyas/pranav/mycookbook/MainRecyclerBasicTest.java
@@ -0,0 +1,57 @@
+package com.vyas.pranav.mycookbook;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.intent.IntentStubber;
+
+import com.vyas.pranav.mycookbook.recyclerutils.MainListAdapter;
+import com.vyas.pranav.mycookbook.recyclerutils.MainListAdapter$MainListItemHolder_ViewBinding;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+
+@RunWith(AndroidJUnit4.class)
+
+public class MainRecyclerBasicTest {
+    //MainListAdapter mAdapter
+    @Rule
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void stubAllExternalIntents() {
+        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
+        // every test run. In this case all external Intents will be blocked.
+        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    }
+
+    @Test
+    public void clickItemToOpenItsRecepie(){
+        onView(withId(R.id.rv_main_frag_main)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        intended(hasComponent(RecepieDescriptionActivity.class.getName()));
+
+        onView(withId(R.id.rv_details_frag_recepie_desc)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        intended(hasComponent(SingleStepActivity.class.getName()));
+        }
+}
